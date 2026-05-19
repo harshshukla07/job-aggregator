15f1f89d61b95ea1d9c7f5b88b5043cc8eb5efb5
Harsh Shukla <harshshukla5810@gmail.com>
2026-05-19 22:34:59 +0530

    chore: initial Spring Boot setup

---
+++ src/main/java/com/jobaggregator/core/JobaggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.jobaggregator.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobaggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobaggregatorApplication.class, args);
+	}
+
+}
